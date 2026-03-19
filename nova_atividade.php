c8249f562be5c306aea8713c10189051e64e1172
Mirella de Oliveira e Quadros <ra2557078@MAC315.local>
2026-03-19 10:12:58 -0300

    outro código

---
+++ nova_atividade.php
@@ -0,0 +1,9 @@
+<?php
+$nome = "Mirella";
+$sobrenome = "Quadros";
+$idade = "15 anos";
+$profissao = "<em>Estudante</em>";
+$nomeCompleto = "<strong>$nome $sobrenome</strong>";
+$mensagem = " Olá, meu nome é $nomeCompleto, eu tenho $idade <br/> Minha profissão é $profissao";
+echo "$mensagem";
+?>

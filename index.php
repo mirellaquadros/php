57dc3dcf7f2397ff3bbe28cd6317dd304507d7ca
Mirella de Oliveira e Quadros <ra2557078@MAC315.local>
2026-03-19 08:21:03 -0300

    primeiro codigo

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+    $cidade = "<strong>Bauru</strong>";
+    $dia = "12";
+    $mes = "março";
+    $ano = "2026";
+    $data = "$cidade, $dia de $mes de $ano";
+    echo "$data"; 
+    /* Quatro variáveis p/ cada informação, 
+    depois criei uma variável data, e chamei.*/
+?>

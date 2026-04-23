49791627eee680670c51fa198df82c58d61ad899
Mirella de Oliveira e Quadros <ra2557078@MacBook-Air-de-MAC316.local>
2026-04-23 08:43:24 -0300

    atividades

---
+++ form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form method="POST" name="form" action="destino.php">
+        <label for="idnome">Nome:</label>
+        <input type="text" name="idnome"><br>
+        <label for="idcelular">Celular:</label>
+        <input type="number" name="idcelular"><br>
+        <label for="idcidade">Cidade:</label>
+        <input type="text" name="idcidade"><br>
+        <button type="submit">Enviar dados</button>
+    </form>
+</body>
+</html>
